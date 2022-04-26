d08d9333f1b1f7e8f19b3b28caeca280c79be80e
KACHINENIVAISHNAVI <104063297+KACHINENIVAISHNAVI@users.noreply.github.com>
2022-04-26 21:05:25 +0530

    Create even-odd-method2.py

---
+++ even-odd-method2.py
@@ -0,0 +1,11 @@
+def number(n):
+    if(n%2==0):
+        return even(n)
+    else:
+        return odd(n)
+def even(n):
+    return  True
+def odd(n):
+    return False
+n=int(input())
+print(number(n))
